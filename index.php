48d9cc9510fa95f28b1d9dbf8daae8313e81144e
sago007 <poul@poulsander.com>
2015-05-31 14:37:15 +0200

    Additional tables added

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+
+/*
+Default sorts
+*/
+if ( !isset($_REQUEST['-sort']) and @$_REQUEST['-table'] == 'oastat_web_games' ){
+    $_REQUEST['-sort'] = $_GET['-sort'] = 'time DESC';
+}
+
+if ( !isset($_REQUEST['-sort']) and @$_REQUEST['-table'] == 'oastat_web_maplist' ){
+    $_REQUEST['-sort'] = $_GET['-sort'] = 'c DESC';
+}
+
 /**
  * File: index.php
  * Description:
@@ -9,3 +21,4 @@
  */
 require_once '../xataface-2.1.2/public-api.php';
 df_init(__FILE__, 'http://oastat.poulsander.com/xataface-2.1.2')->display();
+
