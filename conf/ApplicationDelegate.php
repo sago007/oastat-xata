0b43d28519848a2fea4042d6f5e61ec5f19f96aa
sago007 <poul@poulsander.com>
2015-06-07 19:14:53 +0200

    Removed the left coloumns using a css-file.

--- conf/ApplicationDelegate.php
+++ conf/ApplicationDelegate.php
@@ -3,4 +3,14 @@ class conf_ApplicationDelegate {
     function getPermissions(&$record){
         return Dataface_PermissionsTool::READ_ONLY();
     }
+    
+    public function beforeHandleRequest(){
+    Dataface_Application::getInstance()
+        ->addHeadContent(
+            sprintf('<link rel="stylesheet" type="text/css" href="%s"/>',
+                htmlspecialchars(DATAFACE_SITE_URL.'/css/style.css')
+            )
+        );
+    }
+
 }
