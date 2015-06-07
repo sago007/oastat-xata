409124a252df97122c719e541bab98b99194341c
sago007 <poul@poulsander.com>
2015-06-07 16:54:44 +0200

    Added some interface overrides

---
+++ tables/oastat_web_games/oastat_web_games.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class tables_oastat_web_games {
+    
+    function getTitle(&$record){
+		return 'Game '.$record->val('gamenumber').' on '.$record->val('mapname').' at '.$record->getValueAsString('time');
+	}
+    
+    function section__hello(&$record){
+    return array(
+        'content' => 'Information about the game',
+        'class' => 'main',
+        'order' => '-1'
+    );
+}
+    
+}
