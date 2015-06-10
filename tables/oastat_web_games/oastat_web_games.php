577ab007457ddcbe274e28442b31909539e5ae21
sago007 <poul@poulsander.com>
2015-06-10 20:20:51 +0200

    Added a score table to the game page

--- tables/oastat_web_games/oastat_web_games.php
+++ tables/oastat_web_games/oastat_web_games.php
@@ -14,10 +14,34 @@ class tables_oastat_web_games {
     
     function section__hello(&$record){
     return array(
-        'content' => 'Information about the game',
+        'content' => 'Information about the game'.'<br/><img src="images/oa640x400/'.$record->val('mapname').'.jpg" alt="Levelshot from '.$record->val('mapname').'"/>',
         'class' => 'main',
+        'label' => 'Info',
         'order' => -1
     );
+    }
+    
+    function section__scores(&$record) {
+        $app =& Dataface_Application::getInstance();
+        $conf =& $app->conf();
+        $mysqli = new mysqli($conf['_database']['host'], $conf['_database']['user'], $conf['_database']['password'], $conf['_database']['name']);
+        $gamenumber=$record->val('gamenumber');
+        $stmt = $mysqli->prepare('SELECT p, nick, s FROM oastat_web_scores WHERE gamenumber = ? ORDER BY s DESC');
+        $stmt->bind_param('i', $gamenumber );
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($playerid, $nick, $score);
+        $content = 'Scores: <br/><table>';
+        while($stmt->fetch()) { 
+            $content .= '<tr><td><a href="index.php?-table=oastat_web_players&-action=browse&-recordid=oastat_web_players%3Fplayerid%3D'.$playerid.'">'.$nick.'</a></td><td>'.$score.'</td></tr>';
+        }
+        $content .= '</table>';
+        return array(
+            'content' => $content,
+            'class' => 'main',
+            'label' => 'Scores',
+            'order' => 1
+        );
     }
     
 }
