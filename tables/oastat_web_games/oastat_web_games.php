2d7d2d27519fddf2e40aada80ab428dcc1788cd1
sago007 <poul@poulsander.com>
2015-06-14 19:50:21 +0200

    More info on gamepage

--- tables/oastat_web_games/oastat_web_games.php
+++ tables/oastat_web_games/oastat_web_games.php
@@ -8,6 +8,13 @@
 
 class tables_oastat_web_games {
     
+    var $mysqli;
+    function __construct() {
+        $app =& Dataface_Application::getInstance();
+        $conf =& $app->conf();
+        $this->mysqli = new mysqli($conf['_database']['host'], $conf['_database']['user'], $conf['_database']['password'], $conf['_database']['name']);
+    }
+    
     function getTitle(&$record){
 		return 'Game '.$record->val('gamenumber').' on '.$record->val('mapname').' at '.$record->getValueAsString('time');
 	}
@@ -22,16 +29,15 @@ class tables_oastat_web_games {
     }
     
     function section__scores(&$record) {
-        $app =& Dataface_Application::getInstance();
-        $conf =& $app->conf();
-        $mysqli = new mysqli($conf['_database']['host'], $conf['_database']['user'], $conf['_database']['password'], $conf['_database']['name']);
+        
         $gamenumber=$record->val('gamenumber');
-        $stmt = $mysqli->prepare('SELECT p, nick, s FROM oastat_web_scores WHERE gamenumber = ? ORDER BY s DESC');
+        $stmt = $this->mysqli->prepare('SELECT p, nick, s FROM oastat_web_scores WHERE gamenumber = ? ORDER BY s DESC');
         $stmt->bind_param('i', $gamenumber );
         $stmt->execute();
         $stmt->store_result();
         $stmt->bind_result($playerid, $nick, $score);
         $content = 'Scores: <br/><table>';
+        $content .= '<tr><td>Player</td><td>Score</td></tr>';
         while($stmt->fetch()) { 
             $content .= '<tr><td><a href="index.php?-table=oastat_web_players&-action=browse&-recordid=oastat_web_players%3Fplayerid%3D'.$playerid.'">'.$nick.'</a></td><td>'.$score.'</td></tr>';
         }
@@ -41,6 +47,27 @@ class tables_oastat_web_games {
             'class' => 'main',
             'label' => 'Scores',
             'order' => 1
+        );
+    }
+    
+    function section__weapons(&$record) {
+        $gamenumber=$record->val('gamenumber');
+        $stmt = $this->mysqli->prepare('SELECT w, c FROM oastat_web_kills_by_weapon_gamenumber WHERE gamenumber = ? ORDER BY c DESC');
+        $stmt->bind_param('i', $gamenumber );
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($weapon, $kills);
+        $content = 'Kills by weapon: <br/><table>';
+        $content .= '<tr><td>Weapon</td><td>Kill count</td></tr>';
+        while($stmt->fetch()) { 
+            $content .= '<tr><td>'.$weapon.'</td><td>'.$kills.'</td></tr>';
+        }
+        $content .= '</table>';
+        return array(
+            'content' => $content,
+            'class' => 'main',
+            'label' => 'Kills by weapon',
+            'order' => 2
         );
     }
     
